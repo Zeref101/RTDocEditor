36901fe739f38e024fe0d44319fcdfce1b20bbe4
shreyas <shreyas.mohanty03@gmail.com>
2024-05-29 16:49:12 +0530

    feat: Add middleware for handling authentication redirects

---
+++ middleware.ts
@@ -0,0 +1,23 @@
+import { NextResponse } from "next/server";
+
+export default function middleware(req: any) {
+  let verify = req.cookies.get("pookie");
+  let url = req.url;
+
+  // If the cookie doesn't exist and the current page is the homepage, redirect to "/signUp"
+  if (!verify && url === "http://localhost:3000/") {
+    return NextResponse.redirect("http://localhost:3000/signUp");
+  }
+
+  // If the cookie exists and the current page is either "/signIn" or "/signUp", redirect to the homepage
+  if (
+    verify &&
+    (url === "http://localhost:3000/signIn" ||
+      url === "http://localhost:3000/signUp")
+  ) {
+    return NextResponse.redirect("http://localhost:3000/");
+  }
+
+  // If none of the above conditions are met, don't do any redirect
+  return NextResponse.next();
+}
