860a5a1f3896c024e7e57569b8cd621369878258
shreyas <shreyas.mohanty03@gmail.com>
2024-05-29 16:48:48 +0530

    chore: Refactor sign-in page to use axios for API requests

--- app/(auth)/signIn/[[...signIn]]/page.tsx
+++ app/(auth)/signIn/[[...signIn]]/page.tsx
@@ -1,11 +1,39 @@
 'use client'
-import React from 'react'
+import React, { useState } from 'react'
 import login from "/public/login.svg"
 import Image from 'next/image'
 import google from "/public/google.svg"
 import Link from 'next/link'
+import { useRouter } from 'next/navigation';
+import axios from 'axios'
 
-const page = () => {
+const Page = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [loading, setLoading] = useState(false);
+    const router = useRouter();
+
+    const signIn = async (event: React.FormEvent) => {
+        event.preventDefault();
+        setLoading(true);
+
+        try {
+            const response = await axios.post(`http://localhost:8000/api/auth/signIn`, {
+                email: email,
+                password: password
+            });
+
+            if (response.status === 200) {
+
+                // toast.success('Successfully toasted!')
+                router.push('http://localhost:3000/')
+            }
+        } catch (error) {
+            console.error(error);
+        } finally {
+            setLoading(false);
+        }
+    };
     return (
         <div className=' flex flex-col justify-center items-center w-2/4 '>
             <div className='flex flex-col justify-center items-center'>
@@ -21,18 +49,13 @@ const page = () => {
                     Welcome back! Sign in to continue exploring our amazing features.
                 </p>
             </div>
-            <form action="" className='flex flex-col gap-4 w-3/5 justify-start mt-4 '>
-                <label htmlFor="username"
-                    className='text-[18px] font-medium leading-[25.2px]'
-                >Username <span className=' text-red-600'>*</span></label>
-                <input
-                    type="text"
-                    id='username'
-                    name="username"
-                    placeholder="Username"
-                    required
-                    className=' rounded-md p-3 border border-[#212734] bg-[#0F1117]'
-                />
+            <form action=""
+                className='flex flex-col gap-4 w-3/5 justify-start mt-4 '
+                onSubmit={(e) => {
+                    e.preventDefault();
+                    signIn(e)
+                }}
+            >
 
                 <label htmlFor="email" className='text-[18px] font-medium leading-[25.2px]'>Email <span className=' text-red-600'>*</span></label>
                 <input
@@ -42,6 +65,8 @@ const page = () => {
                     placeholder="Email"
                     required
                     className=' rounded-md p-3 border border-[#212734] bg-[#0F1117]'
+                    onChange={(e) => setEmail(e.target.value)}
+
                 />
 
                 <label htmlFor="password" className='text-[18px] font-medium leading-[25.2px]'>Password <span className=' text-red-600'>*</span></label>
@@ -52,13 +77,16 @@ const page = () => {
                     placeholder="Password"
                     required
                     className=' rounded-md p-3 border border-[#212734] bg-[#0F1117]'
+                    onChange={(e) => setPassword(e.target.value)}
+
                 />
 
-                <input type="submit" value="Sign In" className=' bg-[#d0aae7] mt-8 w-full p-3 border rounded-md text-black cursor-pointer' />
+
+                <input type="submit" value={loading ? "Loading..." : "Sign In"} disabled={loading} className=' bg-[#d0aae7] mt-8 w-full p-3 border rounded-md text-black cursor-pointer' />
                 <div className='separator text-[18px] font-medium leading-[13px]'>or</div>
             </form>
             <button className=' flex items-center justify-center w-3/5 border-2 p-2.5 rounded-md' onClick={() => {
-                console.log("hello")
+                window.location.href = `http://localhost:8000/auth/google`;
             }} >
                 <Image
                     src={google}
@@ -80,4 +108,4 @@ const page = () => {
     )
 }
 
-export default page
+export default Page
