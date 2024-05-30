364f1018535df9774b00ad35ca47516b3cf7d4ae
shreyas <shreyas.mohanty03@gmail.com>
2024-05-30 13:56:16 +0530

    feat: Add new page component with TipTap editor

---
+++ app/(root)/document/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Tiptap from '@/components/editor/TipTap'
+
+const Page = () => {
+    return (
+        <div className='w-screen h-screen'>
+
+
+            <Tiptap />
+        </div>
+    )
+}
+
+export default Page
