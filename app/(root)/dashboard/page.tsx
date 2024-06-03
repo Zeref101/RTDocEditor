5661092162468d89741b7c8a067284eee32c7d4f
shreyas <shreyas.mohanty03@gmail.com>
2024-06-03 13:03:07 +0530

    feat: Add dashboard page component with create document functionality

---
+++ app/(root)/dashboard/page.tsx
@@ -0,0 +1,32 @@
+'use client'
+import React, { useContext } from 'react';
+import axios from 'axios';
+import UserContext from '@/context/UserDetails';
+import { UserContextProps } from '@/context/UserDetails';
+
+const Page = () => {
+    const userContext = useContext(UserContext) as UserContextProps;
+
+    const { user } = userContext;
+
+    const createDocument = async () => {
+        try {
+            const response = await axios.post('http://localhost:8000/api/documents', {
+                owner: user?._id,
+            });
+
+            console.log('Document created:', response.data._id);
+        } catch (error) {
+            console.error('Failed to create document:', error);
+        }
+    };
+
+    return (
+        <div>
+            Dashboard
+            <button onClick={createDocument}>Create Document</button>
+        </div>
+    );
+};
+
+export default Page;
