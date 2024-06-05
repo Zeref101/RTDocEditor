b47f5cae436dffee993819edea2f274a7b57aba1
shreyas <shreyas.mohanty03@gmail.com>
2024-06-05 12:34:47 +0530

    feat: Add createDocument function to services/document.ts

---
+++ services/document.ts
@@ -0,0 +1,14 @@
+import axios from "axios";
+import { UserProps } from "@/types";
+
+export const createDocument = async (user: UserProps) => {
+  try {
+    const response = await axios.post("http://localhost:8000/api/documents", {
+      owner: user?._id,
+    });
+
+    console.log("Document created:", response.data._id);
+  } catch (error) {
+    console.error("Failed to create document:", error);
+  }
+};
